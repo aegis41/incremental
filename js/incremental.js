c337b8d409366984bdd8c1010a5b71d08536b1b7
aegis41 <garrrocks@gmail.com>
2015-07-09 22:41:36 -0400

    Building, Timer, Objects
    Added a building, a button to operate it, a timer that ticks the money
    increase, an object to run the game, and an object to hold building
    data.

--- js/incremental.js
+++ js/incremental.js
@@ -1,7 +1,52 @@
-var money = 0;
+var Timer = window.setInterval(function(){tick()},1000);
+var BuildingName = "Lemonade Stand";
+var BuildingCost = 10;
+var BuildingPerSec = 1;
+var BuildingQty = 0;
+
+window.onload = function()
+{
+	window.game = new GameSave();
+}
+
+window.building1 = new Building();
+building1.name = "Lemonade Stand";
+building1.cost = 10;
+building1.persec = 1;
+building1.qty = 0;
+
+function GameSave()
+{
+	this.money = 0;
+}
 
 function GatherMoney()
 {
-	money = money +1;
-	document.getElementById("money").innerHTML = money;
+	game.money ++;
+	document.getElementById("money").innerHTML = game.money;
+}
+
+function Building() 
+{
+	this.name = "Building Name";
+	this.cost = 10;
+	this.persec = 1;
+	this.qty = 0;
+}
+
+function tick()
+{
+	game.money += 1 + (building1.qty * building1.persec);
+	document.getElementById("money").innerHTML = game.money;
+}
+
+function BuyLemonadeStand()
+{
+	if (game.money >= building1.cost)
+	{
+		game.money -= building1.cost;
+		building1.qty++;
+		document.getElementById("money").innerHTML = game.money;
+		document.getElementById("buildingQty").innerHTML = building1.qty;
+	}
 }
